1f736580199fb3daf9635a938636d88db12893ea
Roma <roma.pholcin@yandex.ru>
2025-04-07 23:40:18 +0600

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Kursach_again
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            field_width = new TextBox();
+            number_of_squares = new TextBox();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(67, 120);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Начать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Ширина поля";
+            // 
+            // field_width
+            // 
+            field_width.Location = new Point(153, 22);
+            field_width.Name = "field_width";
+            field_width.Size = new Size(50, 23);
+            field_width.TabIndex = 2;
+            // 
+            // number_of_squares
+            // 
+            number_of_squares.Location = new Point(152, 66);
+            number_of_squares.Name = "number_of_squares";
+            number_of_squares.Size = new Size(50, 23);
+            number_of_squares.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(11, 69);
+            label3.Name = "label3";
+            label3.Size = new Size(130, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Количество квадартов";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(222, 160);
+            Controls.Add(number_of_squares);
+            Controls.Add(label3);
+            Controls.Add(field_width);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox field_width;
+        private TextBox number_of_squares;
+        private Label label3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+namespace Kursach_again
+{
+    public partial class Form1 : Form
+    {
+        Form field = new Form();
+        List<int[]> field_matrix = new List<int[]>();
+        List<Button> button_list = new List<Button>();
+        List<int[]> coords = new List<int[]>();
+        List<int[]> copy_field_matrix = new List<int[]>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private bool ValidateFields()
+        {
+
+            bool isValid = true;
+
+            if (!(int.TryParse(field_width.Text, out int val)))
+            {
+                isValid = false;
+            }
+
+            if (!(int.TryParse(number_of_squares.Text, out int val2)))
+            {
+                isValid = false;
+            }
+
+
+            return isValid;
+        }
+
+        private int find_touching_sum(int x, int y, int size)
+        {
+            int touch_sum = -1;
+
+            for (int i = 0; i < size; i++)
+            {
+                touch_sum += field_matrix[y + i][x];
+                touch_sum += field_matrix[y][x + i];
+            }
+
+            return touch_sum;
+        }
+
+        private bool check_crosses(int x, int y, int size)
+        {
+            copy_field_matrix.Clear();
+            copy_field_matrix.AddRange(field_matrix);
+            
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i == 0 || j == 0 || i == size - 1 || j == size - 1) copy_field_matrix[y + i][x + j] += 1;
+                    else copy_field_matrix[y + i][x + j] += 2;
+
+                    if (copy_field_matrix[y + i][x + j] > 2) return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void build_optimal_square()
+        {
+            int id = 0, max_sum = 0;
+
+            for (int i = 0; i < coords.Count; i++)
+            {
+                if (coords[i][0] > max_sum)
+                {
+                    max_sum = coords[i][0];
+                    id = i;
+                }
+            }
+
+            int x = coords[id][1], y = coords[id][2], size = coords[id][3];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i == 0 || j == 0 || i == size - 1 || j == size - 1) field_matrix[y + i][x + j] += 1;
+                    else field_matrix[y + i][x + j] += 2;
+                }
+            }
+
+            field_matrix[y + size - 1][x] = 1;
+            field_matrix[y][x + size - 1] = 1;
+            field_matrix[y + size - 1][x + size - 1] = 1;
+
+            //построить кнопку
+
+        }
+
+        private void add_height(int height, int width)
+        {
+            for (int i = 0; i <= height; i++)
+            {
+                int[] new_line = new int[width];
+
+                for (int j = 0; j < width; j++) new_line[j] = 0;
+                new_line[0] = 1;
+                new_line[width - 1] = 1;
+
+                field_matrix.Add(new_line);
+            }
+        }
+
+        private void make_matrix(int width, int height)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                int[] line = new int[width];
+
+                if (i == 0) for (int j = 0; j < width; j++) line[j] = 1;
+                else
+                {
+                    for (int j = 0; j < width; j++) line[j] = 0;
+                    line[0] = 1;
+                    line[width - 1] = 1;
+                }
+
+                field_matrix.Add(line);
+            }
+        }
+
+        private void make_buttons(int amount, int size)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                Button button = new Button();
+                button.Width = size;
+                button.Height = size;
+                button.Text = size.ToString();
+
+                button_list.Add(button);
+            }
+        }
+
+        private void place_buttons(int width)
+        {
+            foreach (Button button in button_list)
+            {
+                coords.Clear();
+
+                for (int y = 0; y < field_matrix.Count; y++)
+                {
+                    if (field_matrix[y].Sum() == 2) break;
+
+                    for (int x = 0; x < width; x++)
+                    {
+                        if (field_matrix[y][x] == 1)
+                        {
+                            if (x + button.Width > width) break;
+
+                            if (y + button.Height > field_matrix.Count) add_height(button.Height, width);
+
+                            if(check_crosses(x, y, button.Width)) break;
+
+                            coords.Add([find_touching_sum(x, y, button.Width), x, y, button.Width]);
+
+                        }
+                    }
+                }
+
+                build_optimal_square();
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!ValidateFields())
+            {
+                MessageBox.Show("NaN");
+                return;
+            }
+
+
+            int squares_count = int.Parse(number_of_squares.Text);
+            int scale = 8;
+            int width = int.Parse(field_width.Text);
+            int height = squares_count * width / 4;
+
+            if (squares_count >= 30 || width >= 30) scale = 2;
+            else if (squares_count >= 25 || width >= 25) scale = 4;
+            else if (squares_count >= 20 || width >= 20) scale = 6;
+
+            make_matrix(width, height);
+            make_buttons(squares_count, width / 4);
+            place_buttons(width);
+
+
+            field.Size = new Size(width * scale, height * scale);
+            field.Show();
+            button1.Enabled = false;
+        }
+    }
+}
